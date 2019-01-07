7b7f60e1cc327e30f6d360ec94f58c2baad049c3
BijeshNair <bijesh4@gmail.com>
2019-01-07 13:39:08 +0530

    Initial Commits

--- Example/BNMenuViewController/ViewController.swift
+++ Example/BNMenuViewController/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController, BNMenuDelegate {
         modalController.modalPresentationStyle = .overCurrentContext
         modalController.delegate = self
         modalController.arrayList = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
-        modalController.arrayIcons = ["1", "2", "3", "4", "5", "6", "", "1", "2", "3", "4", "5", "6"]
+        modalController.arrayIcons = ["1", "2", "3", "4", "5", "6"]
         modalController.iconSize = CGSize(width: 45, height: 45)
         modalController.itemTextColor = .purple
         modalController.itemTextFont = UIFont(name: "Baskerville-SemiBoldItalic", size: 20)!
